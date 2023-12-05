7100bd360bde2311f12dc7163058d9fd5fc28e45
valley30 <adrian.dolinski00@gmail.com>
2023-12-05 23:28:12 +0100

    Added expiry date for token

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, {useEffect, useState} from 'react';
+import jwt_decode, {jwtDecode} from 'jwt-decode';
+
 import LoginForm from './components/LoginForm';
 import WelcomePage from './components/WelcomePage';
 import SideMenu from './components/SideMenu';
@@ -14,27 +16,48 @@ axios.interceptors.request.use((config) => {
 });
 
 function App() {
-
+    const isTokenValid = (token) => {
+        try {
+            const decoded = jwtDecode(token);
+            const currentTime = Date.now() / 1000; // Bieżący czas w sekundach
+            return decoded.exp > currentTime;
+        } catch (error) {
+            return false;
+        }
+    };
     const [user, setUser] = useState(null);
 // App.js
     useEffect(() => {
         const token = localStorage.getItem('jwtToken');
-        if (token) {
-            // Możesz dodać tutaj dodatkową logikę weryfikacji tokenu, np. sprawdzenie daty wygaśnięcia
-            // Zakładamy, że jeśli token istnieje, użytkownik jest zalogowany
-            setUser({ accessToken: token });
+        if (token && isTokenValid(token)) {
+            try {
+                const decoded = jwtDecode(token);
+                setUser({ accessToken: token, data: decoded });
+            } catch (error) {
+                console.log('Błąd dekodowania tokenu:', error);
+                handleLogout();
+            }
+        } else {
+            handleLogout();
         }
     }, []);
 
-
-
-
     const handleLoginSuccess = (userData) => {
-        localStorage.setItem('jwtToken', userData.accessToken); // Zapisz token w Local Storage
-        setUser(userData);
+        localStorage.setItem('jwtToken', userData.accessToken);
+        try {
+            if (isTokenValid(userData.accessToken)) {
+                const decoded = jwtDecode(userData.accessToken);
+                setUser({ accessToken: userData.accessToken, data: decoded });
+            } else {
+                handleLogout();
+            }
+        } catch (error) {
+            console.log('Błąd dekodowania tokenu:', error);
+            handleLogout();
+        }
     };
 
-  const handleLoginFailure = () => {
+    const handleLoginFailure = () => {
     // Obsłuż nieudane logowanie
   };
     const handleLogout = () => {
